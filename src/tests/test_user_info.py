d8592d24d62df67191ffdb61354063d8d66f8c17
saikiranshet <32921574+saikiranshet@users.noreply.github.com>
2023-07-29 08:13:39 +0000

    negative testcases

--- src/tests/test_user_info.py
+++ src/tests/test_user_info.py
@@ -87,3 +87,43 @@ class TestUserInfo:
         for contact in contacts:
             phone_numbers = [contact["value"] for contact in contact["phone_numbers"]]
             assert DATA.PHONE_NUMBER in phone_numbers
+
+    def test_fetch_user_info_with_wrong_phone_number(self, get_api_obj: APICaller):
+        """
+        This is a negative testcase when we provide a number which is wrong
+        """
+        url_offset = f"{USER_INFO.GET_USER_INFO}?order=asc&order_by=created_at&phone_number={DATA.WRONG_NUMBER}"
+        data = get_api_obj.get(url_offset)
+        contacts = data["contacts"]
+        assert isinstance(contacts, list)
+        assert len(contacts) == 0, "The 'contacts' list should be empty."
+
+    def test_fetch_user_info_with_0_phone_number(self, get_api_obj: APICaller):
+        """
+        This is a negative testcase when we provide a number which is 0
+        """
+        url_offset = f"{USER_INFO.GET_USER_INFO}?order=asc&order_by=created_at&phone_number={DATA.ZERO_NUMBER}"
+        data = get_api_obj.get(url_offset)
+        contacts = data["contacts"]
+        assert isinstance(contacts, list)
+        assert len(contacts) == 0, "The 'contacts' list should be empty."
+
+    def test_fetch_user_info_with_negative_phone_number(self, get_api_obj: APICaller):
+        """
+        This is a negative testcase when we provide a number which is -ve
+        """
+        url_offset = f"{USER_INFO.GET_USER_INFO}?order=asc&order_by=created_at&phone_number={DATA.NEGATIVE_NUMBER}"
+        data = get_api_obj.get(url_offset, skip_err=True)
+        status_value = data["errors"][0]["status"]
+        assert status_value == 400
+
+    def test_fetch_user_info_with_aphanumeric_phone_number(
+        self, get_api_obj: APICaller
+    ):
+        """
+        This is a negative testcase when we provide a number which is alphanumeric
+        """
+        url_offset = f"{USER_INFO.GET_USER_INFO}?order=asc&order_by=created_at&phone_number={DATA.ALPHANUMBERIC_NUMBER}"
+        data = get_api_obj.get(url_offset, skip_err=True)
+        status_value = data["errors"][0]["status"]
+        assert status_value == 400
--- src/utils/request_gen.py
+++ src/utils/request_gen.py
@@ -11,15 +11,18 @@ class APICaller:
         url_offest,
         err_key: str = "message",
         error: str = "Failed to make GET request",
+        skip_err = False
     ):
         url = f"{self.base_url}{url_offest}"
         response = requests.get(url, headers=self.headers, timeout=30)
         res = None
         if "application/json" in response.headers.get("Content-Type", ""):
-            res = response.json()
-        if response.status_code >= 400:
+            res = response.json()    
+        if not skip_err and response.status_code >= 400:
             message = error
             if isinstance(res, dict):
                 message = res.get(err_key, error)
             raise Exception(message)
         return res
+      
+        
